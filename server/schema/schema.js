161b0cdc64f62bef73b0c9cbff6d9ca8d29d93f8
Jeffrey <aligula.jeff@gmail.com>
2022-11-13 13:10:19 +0300

    updated schema with disease search query methods

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -18,7 +18,7 @@ const DoctorType = new GraphQLObjectType({
     profession: { type: GraphQLString },
     phone: { type: GraphQLString },
   }),
-  });
+});
 
 // Patient Type
 const PatientType = new GraphQLObjectType({
@@ -36,37 +36,47 @@ const PatientType = new GraphQLObjectType({
   }),
 });
 
-//used to get specific patients from the list of patients
+// Disease Type
+const DiseaseType = new GraphQLObjectType({
+  name: "Disease",
+  fields: () => ({
+    id: { type: GraphQLID },
+    diseaseName: { type: GraphQLString },
+    symptoms: { type: GraphQLString },
+    treatment: { type: GraphQLString },
+  }),
+});
+
+//used to get specific details of doctors, patients or diseases
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-
     //Doctors search methods
-      //used to return a list of Doctors
-      doctors: {
-        type: new GraphQLList(DoctorType),
-        resolve(parent, args) { 
-          return doctors;
-        },
+    //used to return a list of Doctors
+    doctors: {
+      type: new GraphQLList(DoctorType),
+      resolve(parent, args) {
+        return doctors;
       },
-        //used to return individual Doctor
-      doctor: {
-        type: DoctorType,
-        args: { id: { type: GraphQLID } },
-        resolve(parent, args) {
-          return doctors.find((doctor) => doctor.id === args.id);
-        },
     },
-      
-  //Patients search methods
+    //used to return individual Doctor
+    doctor: {
+      type: DoctorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return doctors.find((doctor) => doctor.id === args.id);
+      },
+    },
+
+    //Patients search methods
     //used to return a list of patients
     patients: {
       type: new GraphQLList(PatientType),
-      resolve(parent, args) { 
+      resolve(parent, args) {
         return patients;
       },
     },
-      //used to return individual patient
+    //used to return individual patient
     patient: {
       type: PatientType,
       args: { id: { type: GraphQLID } },
@@ -74,8 +84,25 @@ const RootQuery = new GraphQLObjectType({
         return patients.find((patient) => patient.id === args.id);
       },
     },
+
+    //Disease search methods
+    //used to return a list of diseases
+    diseases: {
+      type: new GraphQLList(DiseaseType),
+      resolve(parent, args) {
+        return diseases;
+      },
+    },
+    //used to return individual diseases details
+    disease: {
+      type: DiseaseType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return diseases.find((disease) => disease.id === args.id);
+      },
+    },
   },
-}); 
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
